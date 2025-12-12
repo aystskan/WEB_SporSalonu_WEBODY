26d97367d36986313a40d3ef96436df674fbee86
Ayşe TAŞKAN <ays.taskan@gmail.com>
2025-12-12 14:00:00 +0300

    Add files via upload

---
+++ WEBODY/Models/Antrenor.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations; //Doğrulama için gerekli namespace
+
+namespace WEBODY.Models
+{
+    public class Antrenor
+    {
+        [Key] //Primary Key
+        public int AntrenorId { get; set; }
+
+        [Required(ErrorMessage = "Ad Soyad alanı zorunludur.")] //Zorunlu alan
+        [StringLength(100, ErrorMessage = "En fazla 100 karakter girebilirsiniz.")] // PDF Sayfa 82
+        [Display(Name = "Antrenör Adı Soyadı")]
+        public string AdSoyad { get; set; }
+
+        [Required]
+        [Display(Name = "Uzmanlık Alanı")]
+        public string UzmanlikAlani { get; set; } // Örn: Yoga, Pilates, BodyBuilding
+
+        // Bir antrenörün birden fazla randevusu olabilir (Navigation Property - PDF Sayfa 108)
+        public ICollection<Randevu> Randevular { get; set; }
+    }
+}
+
+
